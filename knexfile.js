908228c1bf10b18b39196b050cb6fb7e5928d9d4
AriosiLucas <ariosilucas@gmail.com>
2024-06-18 21:09:25 -0300

    Configuração Inicial projeto

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+
+module.exports = {
+    development: {
+        client: "mysql",
+        connection: {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            database: process.env.DB_NAME,
+            password: process.env.DB_PASSWORD,
+        },
+        migrations: {
+            directory: path.resolve(
+                __dirname,
+                "src",
+                "database",
+                "knex",
+                "migrations"
+            ),
+        },
+        useNullAsDefault: true,
+    },
+};

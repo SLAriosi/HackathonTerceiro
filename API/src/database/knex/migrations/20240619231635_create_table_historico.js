070c44787086665c37695b7d405a3a61547c9ce7
Arthur <arthurtomba@gmail.com>
2024-06-20 19:39:46 -0300

    Altera migration, adiciona controller e rotas agendamento

--- API/src/database/knex/migrations/20240619231635_create_table_historico.js
+++ API/src/database/knex/migrations/20240619231635_create_table_historico.js
@@ -1,41 +1,29 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-   return knex.schema.hasTable("agendamento_vacina").then((exists) => {
-     if (!exists) {
-       return knex.schema.createTable("agendamento_vacina", (table) => {
-         table.bigIncrements("id");
-         table.date("data").notNullable();
-         table.time("horario").notNullable();
-         table.bigInteger("vacina_id").unsigned().notNullable();
-         table.bigInteger("idoso_id").unsigned().notNullable();
-         table.timestamp("created_at").defaultTo(knex.fn.now());
-         table.timestamp("updated_at").defaultTo(knex.fn.now());
-         
-         table.foreign("vacina_id").references("id").inTable("vacina");
-         table.foreign("idoso_id").references("id").inTable("idoso");
-       })
-       .then(() => {
-         console.log("Criada tabela de Agendamento de Vacinas");
-      });
-     }
-   });
- };
- 
- exports.down = async function(knex) {
-   await knex.schema.dropTable("agendamento_vacina")
-   .then((result) => {
-      console.log("Deletada tabela de Agendamento de Vacinas");
-   })
- };
- 
+exports.up = function (knex) {
+  return knex.schema.hasTable("agendamento_vacina").then((exists) => {
+    if (!exists) {
+      return knex.schema.createTable("agendamento_vacina", (table) => {
+        table.bigIncrements("id");
+        table.date("data").notNullable();
+        table.string("horario").notNullable();
+        table.string("vacina", 100).notNullable();
+        table.string("nome", 100).notNullable();
+        table.string("cpf", 11).notNullable();
+        table.string("cep", 8).notNullable();
+        table.date("data_nascimento").notNullable();
+        table.string("telefone", 11).notNullable();
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+        table.timestamp("updated_at").defaultTo(knex.fn.now());
+      })
+        .then(() => {
+          console.log("Criada tabela de Agendamento de Vacinas");
+        });
+    }
+  });
+};
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+  await knex.schema.dropTable("agendamento_vacina")
+    .then((result) => {
+      console.log("Deletada tabela de Agendamento de Vacinas");
+    })
 };

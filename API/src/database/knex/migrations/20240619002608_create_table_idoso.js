21bc268486aadbe948152d204951218c7fb2f28f
AriosiLucas <ariosilucas@gmail.com>
2024-06-21 19:14:19 -0300

    Arruma migrations

--- API/src/database/knex/migrations/20240619002608_create_table_idoso.js
+++ API/src/database/knex/migrations/20240619002608_create_table_idoso.js
@@ -7,17 +7,12 @@ exports.up = async function (knex) {
       .createTable("idoso", (table) => {
          table.bigIncrements("id");
          table.string("nome", 100).notNullable();
-         table.string("telefone", 11).notNullable();
-         table.date("data_nascimento").notNullable();
          table.string("cpf", 11).notNullable();
          table.string("cep", 8).notNullable();
+         table.string("telefone", 11).notNullable();
          table.integer("numero_casa").notNullable();
-         table.string("email", 100).notNullable();
-         table.string("rua", 100).notNullable();
-         table.string("bairro", 100).notNullable();
-         table.string("cidade", 100).notNullable();
-         table.string("cartao_sus", 16).notNullable();
          table.text("condicoes");
+         table.date("data_nascimento").notNullable();
          table.timestamp("created_at").defaultTo(knex.fn.now());
          table.timestamp("updated_at").defaultTo(knex.fn.now());
       })
